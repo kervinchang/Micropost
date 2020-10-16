9dd90237f09659ea2cd788e76203632e2b7443de
kerwin <zhangcy.cn@outlook.com>
2020-10-16 13:54:00 +0800

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   # before_save { self.email = email.downcase }
   # before_save {email.downcase!}
   before_save   :downcase_email
@@ -51,6 +51,24 @@ class User < ApplicationRecord
   # 发送激活邮件
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # 设置密码重设相关的属性
+  def create_reset_digest
+    self.reset_token = User.new_token
+    # update_attribute(:reset_digest, User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # 发送密码重设邮件
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # 如果密码重设请求超时了， 返回true
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
